ff96f7cb9ea2c1fc183bcfa3b2351b2a452ec017
Magual27 <miguenrique27@gmail.com>
2023-06-07 09:55:06 -0300

    atualização atividade aluno

--- atividade_aluno/src/Main.java
+++ atividade_aluno/src/Main.java
@@ -10,35 +10,51 @@ public class Main {
 
         double n1;
         double n2;
+        boolean i = true;
 
         System.out.println("-------Calculadora-------");
 
-        System.out.print("Insira o 1° número: ");
-        n1 = leitor.nextDouble();
+        while (i) {
 
-        System.out.print("Insira o 2° número: ");
-        n2 = leitor.nextDouble();
+            System.out.print("Insira o 1° número: ");
+            n1 = leitor.nextDouble();
 
-        System.out.println("-------Selecione alguma das opcões-------");
-        System.out.println("1 - soma\n2 - subtração\n3 - multiplicação\n4 - divisão\n5 - tabuada");
-        System.out.print("---> ");
-        int res = leitor.nextInt();
+            System.out.print("Insira o 2° número: ");
+            n2 = leitor.nextDouble();
 
-        if (res == 1) {
-            System.out.print("resultado -> ");
-            aluno1.soma(n1, n2);
-        } else if (res == 2) {
-            System.out.print("resultado -> ");
-            aluno1.sub(n1, n2);
-        } else if (res == 3) {
-            System.out.print("resultado -> ");
-            aluno1.multi(n1, n2);
-        } else if (res == 4) {
-            System.out.print("resultado -> ");
-            aluno1.div(n1, n2);
-        } else if (res == 5) {
-            System.out.println("---resultado---");
-            aluno1.calcTabuada((int) n1);
+            System.out.println("-------Selecione alguma das opcões-------");
+            System.out.println("1 - soma\n2 - subtração\n3 - multiplicação\n4 - divisão\n5 - tabuada");
+            System.out.print("---> ");
+            int res = leitor.nextInt();
+
+            if (res == 1) {
+                System.out.print("resultado -> ");
+                aluno1.soma(n1, n2);
+            } else if (res == 2) {
+                System.out.print("resultado -> ");
+                aluno1.sub(n1, n2);
+            } else if (res == 3) {
+                System.out.print("resultado -> ");
+                aluno1.multi(n1, n2);
+            } else if (res == 4) {
+                System.out.print("resultado -> ");
+                aluno1.div(n1, n2);
+            } else if (res == 5) {
+                System.out.println("---resultado---");
+                aluno1.calcTabuada((int) n1);
+            }
+
+            i = false;
+
+            System.out.println("Deseja usar de novo?\n1 - sim\n2 - finalizar programa");
+            int opcao = leitor.nextInt();
+
+            if (opcao == 1) {
+                i = true;
+            } else if (opcao == 2) {
+                i = false;
+            }
+
         }
 
     }
