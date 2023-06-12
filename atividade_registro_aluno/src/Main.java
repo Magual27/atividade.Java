f4c7359e70f5d013be7386e1794d12df3aa9b54e
Magual27 <miguenrique27@gmail.com>
2023-06-12 12:13:47 -0300

    atualizão atividade aluno_registtro

--- atividade_registro_aluno/src/Main.java
+++ atividade_registro_aluno/src/Main.java
@@ -27,7 +27,7 @@ public class Main {
             switch (opcao) {
                 case 1 -> {
                     opcao = 0;
-                    System.out.print("O que você deseja fazer? \n1 - Adicionar aluno\n2 - Consultar aluno\n3 - Mostrar alunos cadastrados\n---> ");
+                    System.out.print("O que você deseja fazer? \n1 - Adicionar aluno\n2 - Consultar aluno\n3 - Mostrar alunos cadastrados\n4 - Atualizar nome\n---> ");
                     opcao = sc.nextInt();
                     switch (opcao) {
                         case 1 -> {
@@ -51,11 +51,33 @@ public class Main {
                                 System.out.printf("%06d %-14s \n", a.getId(), a.getNome());
                             }
                         }
+                        case 4 -> {
+                            System.out.print("Digite o nome desse aluno que desesa atualizar\n---> ");
+                            nomeProcurado = sc.next();
+                            System.out.print("Digite o novo nome desse aluno\n---> ");
+                            String novoNome = sc.next();
+                            for (Aluno a: alunos) {
+                                if (a.nome.equals(nomeProcurado)) {
+                                    a.setNome(novoNome);
+                                    System.out.printf("Nome atualizado com sucesso! \n"+ "%06d %-14s \n", a.getId(), a.getNome());
+                                }
+                            }
+                        }
+                        case 5 -> {
+                            System.out.print("Digite o nome desse aluno que deseja remover\n---> ");
+                            nomeProcurado = sc.next();
+                            for (Aluno a: alunos) {
+                                if (a.nome.equals(nomeProcurado)) {
+                                    professores.remove(a);
+                                    System.out.printf("Aluno removido com sucesso! \n"+ "%06d %-14s \n", a.getId(), a.getNome());
+                                }
+                            }
+                        }
                     }
                 }
                 case 2 -> {
                     opcao = 0;
-                    System.out.print("O que você deseja fazer? \n1 - Adicionar professor\n2 - Consultar professor\n3 - Mostrar professores cadastrados\n---> ");
+                    System.out.print("O que você deseja fazer? \n1 - Adicionar professor\n2 - Consultar professor\n3 - Mostrar professores cadastrados\n4 - Atualizar nome\n---> ");
                     opcao = sc.nextInt();
                     switch (opcao) {
                         case 1 -> {
@@ -82,11 +104,33 @@ public class Main {
                                 System.out.printf("%06d %-14s \n", a.getId(), a.getNome());
                             }
                         }
+                        case 4 -> {
+                            System.out.print("Digite o nome desse professor que desesa atualizar\n---> ");
+                            nomeProcurado = sc.next();
+                            System.out.print("Digite o novo nome desse professor\n---> ");
+                            String novoNome = sc.next();
+                            for (Professor a: professores) {
+                                if (a.nome.equals(nomeProcurado)) {
+                                    a.setNome(novoNome);
+                                    System.out.printf("Nome atualizado com sucesso! \n"+ "%06d %-14s \n", a.getId(), a.getNome());
+                                }
+                            }
+                        }
+                        case 5 -> {
+                            System.out.print("Digite o nome desse professor que deseja remover\n---> ");
+                            nomeProcurado = sc.next();
+                            for (Aluno a: alunos) {
+                                if (a.nome.equals(nomeProcurado)) {
+                                    professores.remove(a);
+                                    System.out.printf("Professor removido com sucesso! \n"+ "%06d %-14s \n", a.getId(), a.getNome());
+                                }
+                            }
+                        }
                     }
                 }
                 case 3 -> {
                     opcao = 0;
-                    System.out.print("O que você deseja fazer ?\n1 - Adicionar sala\n2 - Consultar sala\n3 - Mostrar salas cadastrados\n---> ");
+                    System.out.print("O que você deseja fazer ?\n1 - Adicionar sala\n2 - Consultar sala\n3 - Mostrar salas cadastrados\n4 - Atualizar nome\n---> ");
                     opcao = sc.nextInt();
                     switch (opcao) {
                         case 1 -> {
@@ -113,6 +157,28 @@ public class Main {
                         case 3 -> {
                             for (Sala a : salas) {
                                 System.out.printf("%06d %-14s \n", a.getId(), a.getNomeSala());
+                            }
+                        }
+                        case 4 -> {
+                            System.out.print("Digite o nome dessa saas que desesa atualizar\n---> ");
+                            nomeProcurado = sc.next();
+                            System.out.print("Digite o novo nome dessa saas\n---> ");
+                            String novoNome = sc.next();
+                            for (Sala a: salas) {
+                                if (a.nomeSala.equals(nomeProcurado)) {
+                                    a.setNomeSala(novoNome);
+                                    System.out.printf("Nome da saas atualizado com sucesso! \n" + "%06d %-14s \n", a.getId(), a.getNomeSala());
+                                }
+                            }
+                        }
+                        case 5 -> {
+                            System.out.print("Digite o nome dessa sala que deseja remover\n---> ");
+                            nomeProcurado = sc.next();
+                            for (Aluno a: alunos) {
+                                if (a.nome.equals(nomeProcurado)) {
+                                    professores.remove(a);
+                                    System.out.printf("Sala removido com sucesso! \n"+ "%06d %-14s \n", a.getId(), a.getNome());
+                                }
                             }
                         }
                     }
